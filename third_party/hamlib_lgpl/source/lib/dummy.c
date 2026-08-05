735d7ce53dd4f6b4058ee1cec6099a6bfdcb0b68
l-papadopol <117158814+l-papadopol@users.noreply.github.com>
2026-08-05 18:48:30 +0200

    clean

--- third_party/hamlib_lgpl/source/lib/dummy.c
+++
@@ -1,9 +0,0 @@
-/* Dummy function to make sure libmisc never become an empty library.
- * Solaris linker does not like such libs.
- */
-// cppcheck-suppress unusedFunction
-void dummy(void)
-{
-
-}
-
